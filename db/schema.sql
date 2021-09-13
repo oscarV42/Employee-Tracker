2207d3b2dc483027f13f6d2cf513b88e28fd742a
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-09-13 16:09:03 -0400

    creating role table in employees database in schema file

--- db/schema.sql
+++ db/schema.sql
@@ -8,3 +8,13 @@ CREATE TABLE department (
     name VARCHAR(30) NOT NULL
 );
 
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_Id INT,
+    FOREIGN KEY (department_Id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
