5f093ff9c64b3d5a72ec8794ce759db7b4bd792c
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-09-14 16:14:39 -0400

    creating choice handler function to determine what happens depending on the users choice when the program starts

--- index.js
+++ index.js
@@ -1,10 +1,50 @@
 const inquirer = require('inquirer');
 
-const question = [
+const questions = [
     {
-        
+        type: 'input',
+        message: 'Please enter the name of the department:',
+        name: 'department_name'  
+    },
+    {
+        type: 'input',
+        message: 'Please enter role name:',
+        name: 'role_name'
+    },
+    {
+        type: 'input',
+        message: 'Enter salary:',
+        name: 'role_salary'
+    },
+    {
+
     },
 ];
+
+function choice_handler(answer){
+    switch(answer) {
+        case 'View all departments':
+            break;
+        case 'View all roles':
+            break;
+        case 'View all employees':
+            break;
+        case 'Add a department':
+            inquirer.prompt(questions.department)
+            .then((answer) =>{
+                console.log(answer)
+            })
+            break;
+        case 'Add a role':
+            break;
+        case 'Add an employee':
+            break;
+        case 'Update an employee role':
+            break; 
+            default:
+                break;
+    }   
+}
 
 // Function to initialize app
 function init() {
@@ -20,26 +60,12 @@ function init() {
          'Add a department', 
          'Add a role', 
          'Add an employee', 
-         'Update an employee role']
+         'Update an employee role',
+         'Quit']
         }
     ])
     .then((data) => {
-        switch(data) {
-            case 'View all departments':
-                break;
-            case 'View all roles':
-                break;
-            case 'View all employees':
-                break;
-            case 'Add a department':
-                break;
-            case 'Add a role':
-                break;
-            case 'Add an employee':
-                break;
-            case 'Update an employee role':
-                break; 
-        }   
+        choice_handler(data.init);
     });
     
 }
