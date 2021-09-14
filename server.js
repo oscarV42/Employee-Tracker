77c119da010b7edf0882f67553f34554d9596d87
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-09-14 03:12:18 -0400

    express middleware

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
 const express = require('express');
 // Import and require mysql2
 const mysql = require('mysql2');
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
