787fc6d59e962e0e823dc568241c940c7f8269e4
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-09-14 03:51:48 -0400

    implementing switch case to determine what happens wihtin the program after the user selects a choice

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const inquirer = require('inquirer');
 
+const question = [
+    {
+        
+    },
+];
+
 // Function to initialize app
 function init() {
     inquirer
@@ -17,6 +23,25 @@ function init() {
          'Update an employee role']
         }
     ])
+    .then((data) => {
+        switch(data) {
+            case 'View all departments':
+                break;
+            case 'View all roles':
+                break;
+            case 'View all employees':
+                break;
+            case 'Add a department':
+                break;
+            case 'Add a role':
+                break;
+            case 'Add an employee':
+                break;
+            case 'Update an employee role':
+                break; 
+        }   
+    });
+    
 }
 
 // Function call to initialize app
