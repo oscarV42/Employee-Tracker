14362cfd5385f7f923e9521b25a21372c908c110
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-09-16 00:01:15 -0400

    creating prompts for each case in which the user is posting something new to the database

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const inquirer = require('inquirer');
 
-const questions = [
+
+const department = [
     {
         type: 'input',
         message: 'Please enter the name of the department:',
         name: 'department_name'  
-    },
+    }
+]
+const role = [
     {
         type: 'input',
         message: 'Please enter role name:',
@@ -17,9 +20,34 @@ const questions = [
         name: 'role_salary'
     },
     {
-
+        type: 'input',
+        message: 'Please enter department id:',
+        name: 'department_id'
+        },
+]
+const employee = [
+    {
+        type: 'input',
+        message: `Enter the employee's first name:`,
+        name: 'first_name'
     },
-];
+    {
+        type: 'input',
+        message: `Enter the employees last name:`,
+        name: 'last_name'
+    },
+    {
+        type: 'input',
+        message: `Enter the employee's role id:`,
+        name: 'role_id'
+    },
+    {
+        type: 'input',
+        message: `Enter the employee's manager id:`,
+        name: 'manager_id'
+    },
+]
+
 
 function choice_handler(answer){
     switch(answer) {
@@ -30,14 +58,22 @@ function choice_handler(answer){
         case 'View all employees':
             break;
         case 'Add a department':
-            inquirer.prompt(questions.department)
-            .then((answer) =>{
-                console.log(answer)
+            inquirer.prompt(department)
+            .then((answer) => {
+                console.log(answer.department_name)
             })
             break;
         case 'Add a role':
+            inquirer.prompt(role)
+            .then((answer) => {
+                console.log(answer)
+            })
             break;
         case 'Add an employee':
+            inquirer.prompt(employee)
+            .then((answer) => {
+                console.log(answer)
+            })
             break;
         case 'Update an employee role':
             break; 
