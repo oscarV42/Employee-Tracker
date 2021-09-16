c0b485d0b5ef5c77aa6b330138854528a121c17b
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-09-16 15:53:51 -0400

    changing file layout

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 // Import and require mysql2
 const mysql = require('mysql2');
+console.log(require('dotenv').config());
+
+const PORT = process.env.PORT || 3001;
+const app = express();
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
@@ -11,11 +15,15 @@ const db = mysql.createConnection(
     {
       host: 'localhost',
       // MySQL username,
-      user: 'root',
+      user: process.env.DB_USER,
       // TODO: Add MySQL password here
-      password: '',
-      database: 'employees_db',
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
     },
     console.log(`Connected to the  database.`)
   );
+  
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
   
