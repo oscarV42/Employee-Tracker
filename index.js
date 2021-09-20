0a02e797dca5b448fdae0fa349150091f51ad9d0
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-09-19 22:18:06 -0400

    deleting some unnecessary required npm dependencies

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 const inquirer = require('inquirer');
-const express = require('express');
 // Import and require mysql2
 const mysql = require('mysql2');
 require('dotenv').config();
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
